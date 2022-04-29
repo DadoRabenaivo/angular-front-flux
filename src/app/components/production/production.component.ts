048d361d9a0270e8ba9cefdf3049ebd0467acbeb
Harry Michael <ando.lalaina1103@gmail.com>
2022-04-29 08:18:49 +0300

    Add table overview with mock data

--- src/app/components/production/production.component.ts
+++ src/app/components/production/production.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdProcess } from 'src/app/interfaces/prodProcess';
 
 @Component({
   selector: 'app-production',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./production.component.css']
 })
 export class ProductionComponent implements OnInit {
+  prodProcesses : Array<ProdProcess> = []
 
   constructor() { }
 
   ngOnInit(): void {
+    this.prodProcesses.push(
+      {
+        Name: "Croissant",
+        InStock: 84,
+        MaterialName: "Micro-onde",
+        MaterialUsability: "24%",
+        Quantity: 54,
+        QuantitySMA: 125
+    },
+      {
+        Name: "Croissant",
+        InStock: 84,
+        MaterialName: "Micro-onde",
+        MaterialUsability: "24%",
+        Quantity: 54,
+        QuantitySMA: 125
+    },
+      {
+        Name: "Croissant",
+        InStock: 84,
+        MaterialName: "Micro-onde",
+        MaterialUsability: "24%",
+        Quantity: 54,
+        QuantitySMA: 125
+    },
+    )
   }
 
 }
---
+++ src/app/interfaces/prodProcess.ts
@@ -0,0 +1,8 @@
+export interface ProdProcess {
+    Name: string,
+    InStock: number,
+    MaterialName: string,
+    MaterialUsability: string,
+    Quantity: number,
+    QuantitySMA: number
+}
